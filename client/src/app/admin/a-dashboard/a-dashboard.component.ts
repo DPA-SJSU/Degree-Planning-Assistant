1958ab82b28c7e6d40ede4d1e6c21f431ec305ed
Elana Olson <elanaolson@gmail.com>
2020-04-13 14:05:53 -0700

    create and get a program in service

--- client/src/app/admin/a-dashboard/a-dashboard.component.ts
+++ client/src/app/admin/a-dashboard/a-dashboard.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProgramData, ProgramService } from "src/app/program.service";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-a-dashboard',
-  templateUrl: './a-dashboard.component.html',
-  styleUrls: ['./a-dashboard.component.css']
+  selector: "app-a-dashboard",
+  templateUrl: "./a-dashboard.component.html",
+  styleUrls: ["./a-dashboard.component.css"],
 })
 export class ADashboardComponent implements OnInit {
+  newProgram: Observable<ProgramData>;
+  program: Observable<[ProgramData]>;
+  constructor(private programService: ProgramService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  createNewProgram() {
+    const program: ProgramData = {
+      school: "SJSU",
+      major: "Chemical Engineering",
+      catalogYear: 2016,
+    };
+    this.newProgram = this.programService.createProgram(program);
   }
 
+  getNewProgram() {
+    this.program = this.programService.getProgram({ school: "SJSU" });
+  }
 }
