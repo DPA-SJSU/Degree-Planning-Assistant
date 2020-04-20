03c5862294007c1ef080691ebb11c83fdd500162
Calvin Nguyen <cuongvqnguyen@gmail.com>
2020-04-20 14:32:55 -0700

    update script for dev & prod (#49)

--- server/store/config.js
+++ server/store/config.js
@@ -1,16 +1,26 @@
+const PROD = 'prod';
 export const config = {
   passport: {
     secret: 'dpa-developers-dec-fall@2019',
     expiresIn: 1000000,
   },
-  env: {
-    port: 8080,
-    mongoDBUri:
-      process.env.ENV === 'prod'
-        ? 'mongodb+srv://team31:w1TNAFsawdQKABWJ@dpa-jmd2c.gcp.mongodb.net/test?retryWrites=true&w=majority'
-        : 'mongodb://localhost/dpa',
-    mongoHostName: process.env.ENV === 'prod' ? 'mongodbAtlas' : 'localhost',
-  },
+  env:
+    process.env.ENV === PROD
+      ? {
+          port: 8080,
+          mongoHostName: 'MongoDB Atlas',
+          mongoDBUri:
+            'mongodb+srv://team31:w1TNAFsawdQKABWJ@dpa-jmd2c.gcp.mongodb.net/test?retryWrites=true&w=majority',
+          clientURI: 'https://localhost:3000',
+          serverURI: 'https://dpa-edu-server.wn.r.appspot.com',
+        }
+      : {
+          port: 8080,
+          mongoHostName: 'local',
+          mongoDBUri: 'mongodb://localhost/dpa',
+          clientURI: 'https://localhost:3000',
+          serverURI: 'http://localhost:8080',
+        },
 };
 
 export const underscoreId = '_id';
